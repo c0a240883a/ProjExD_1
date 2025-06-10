8f8ab7fdb39093cdac0a759e7906d32dc4f59a7d
c0a240883a <c0a240883a@edu.teu.ac.jp>
2025-06-10 15:20:34 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/3.png")
+    bg_img2 = pg.transform.flip(bg_img2,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
